250fa85715b8f261010b9e179ea208d6d88a5710
Anatoliy Tokarev <tokarev182@yandex.ru>
2023-09-18 17:51:51 +0300

    feat: add prop className input

--- src/app/global/App.jsx
+++ src/app/global/App.jsx
@@ -4,6 +4,32 @@ import { Input } from "../../components/Input/Input";
 import { Container } from "../../layout/Container/Container";
 
 function App() {
+  const variantsOptions = [
+    {
+      value: "monthly-payment",
+      text: "Расчет ежемесячного платежа",
+    },
+    {
+      value: "maximum-loan-amount",
+      text: "Расчет максимальной суммы кредита",
+    },
+    {
+      value: "credit-term",
+      text: "Расчет срока кредита",
+    },
+  ];
+
+  const monthOrYearOptions = [
+    {
+      value: "year",
+      text: "Лет",
+    },
+    {
+      value: "month",
+      text: "Месяцев",
+    },
+  ];
+
   return (
     <div className="App">
       <Container>
@@ -11,7 +37,16 @@ function App() {
         <form className="form" action="#">
           <div className="wrapper">
             <div className="wrapper__top">
-              <Input initialValue="10000" label="Ставка" type="text" />
+              <Input
+                className="calculation-option"
+                label="Вариант расчета"
+                list={variantsOptions}
+              />
+              <Input label="Сумма кредита" initialValue="1000000" type="text" />
+              <Input label="Ставка" initialValue="10" type="text" />
+              <Input label="Начало выплат" type="date" />
+              <Input list={monthOrYearOptions} />
+              <Input label="Срок кредита" initialValue="12" type="text" />
             </div>
             <div className="wrapper__bottom"></div>
           </div>
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,22 +1,60 @@
 import React, { useState } from "react";
 import s from "./Input.module.scss";
 
-export const Input = ({ list, input, date, initialValue, label, type }) => {
-  // Используйте initialValue из пропсов для установки начального значения состояния
-  const [inputValue, setInputValue] = useState(initialValue);
+export const Input = ({ label, list, initialValue, type, className }) => {
+  const [inputValue, setInputValue] = useState(initialValue); // initialValue из пропсов для установки начального значения состояния
+  const [isListOpen, setIsListOpen] = useState(false); // Состояние для открытия/закрытия списка list
+  const [selectedItem, setSelectedItem] = useState(null); // Состояние для хранения выбранного элемента из списка list
 
-  // Функция обратного вызова для обновления значения <input>
+  // Обновяление значения input value
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
+  // Меняем значение isListOpen при каждом клике
+  const toggleList = () => {
+    setIsListOpen(!isListOpen);
+  };
+
+  //  Выбираем значение из выпадаюцего списка и закрываем список
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    setIsListOpen(false);
+  };
+
+  // Проверяем, что list существует и является массивом
+  const renderedList =
+    isListOpen && list && Array.isArray(list) ? (
+      <ul className={s.list}>
+        {list.map((item, index) => (
+          <li
+            data-variant={item.variant}
+            key={index}
+            onClick={() => handleItemClick(item)}
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    ) : null;
+
+  const inputClassNames = [s.input, className].join(" "); // Объединяем классы в строку если приходят доп.классы через пропсы
+
   return (
-    <label className={s.input}>
-      <span>{label}</span>
-      {/* сделать условный рендеринг */}
-      <input type={type} value={inputValue} onChange={handleInputChange} />
-      <p>Месяцев</p>
-      <ul></ul>
+    <label className={inputClassNames} onClick={toggleList}>
+      {label && <span>{label}</span>}
+      {list ? (
+        <>
+          <p>{selectedItem ? selectedItem.text : list[0].text}</p>
+          {renderedList}
+        </>
+      ) : (
+        <input
+          type={type === "date" ? "date" : type}
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      )}
     </label>
   );
 };
